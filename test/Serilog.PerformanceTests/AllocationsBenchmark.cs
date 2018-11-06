ebc3d508ef4946d9324306384690dba8a2d78431
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-11-05 23:46:47 -0800

    Improve AllocationsBenchmark

--- test/Serilog.PerformanceTests/AllocationsBenchmark.cs
+++ test/Serilog.PerformanceTests/AllocationsBenchmark.cs
@@ -18,12 +18,16 @@ namespace Serilog.PerformanceTests
         readonly ILogger _logger;
         readonly ILogger _enrichedLogger;
         readonly LogEvent _emptyEvent;
+
         readonly object _dictionaryValue;
         readonly object _anonymousObject;
         readonly object _sequence;
+        readonly Exception _exception;
 
         public AllocationsBenchmark()
         {
+            _exception = new Exception("An Error");
+
             _logger = new LoggerConfiguration().CreateLogger();
 
             _enrichedLogger = _logger.ForContext(new PropertyEnricher("Prop", "Value"));
@@ -96,6 +100,12 @@ namespace Serilog.PerformanceTests
         }
 
         [Benchmark]
+        public void LogScalarStruct()
+        {
+            _logger.Information("Template: {ScalarStructValue}", 42);
+        }
+
+        [Benchmark]
         public void LogDictionary()
         {
             _logger.Information("Template: {DictionaryValue}", _dictionaryValue);
@@ -111,6 +121,46 @@ namespace Serilog.PerformanceTests
         public void LogAnonymous()
         {
             _logger.Information("Template: {@AnonymousObject}.", _anonymousObject);
+        }
+
+        [Benchmark]
+        public bool LogAll()
+        {
+            var x = _logger.IsEnabled(LogEventLevel.Debug);
+            _logger.Write(_emptyEvent);
+            _logger.Information("Template:");
+            _logger.Information("Template: {ScalarStructValue}", 42);
+            _logger.Information("Template: {ScalarValue}", "42");
+            _logger.Information("Template: {DictionaryValue}", _dictionaryValue);
+            _logger.Information("Template: {SequenceValue}", _sequence);
+            _logger.Information("Template: {@AnonymousObject}.", _anonymousObject);
+            _logger.Information(_exception, "Hello, {Name}!", "World");
+            _logger.Fatal("Template2:");
+            _logger.Fatal("Template: {ScalarStructValue}", 42);
+            _logger.Fatal("Template2: {ScalarValue}", "42");
+            _logger.Fatal("Template2: {DictionaryValue}", _dictionaryValue);
+            _logger.Fatal("Template2: {SequenceValue}", _sequence);
+            _logger.Fatal("Template2: {@AnonymousObject}.", _anonymousObject);
+            _logger.Fatal(_exception, "Hello, {Name}! 2", "World");
+
+            var y = _enrichedLogger.IsEnabled(LogEventLevel.Debug);
+            _enrichedLogger.Write(_emptyEvent);
+            _enrichedLogger.Information("Template:");
+            _enrichedLogger.Information("Template: {ScalarStructValue}", 42);
+            _enrichedLogger.Information("Template: {ScalarValue}", "42");
+            _enrichedLogger.Information("Template: {DictionaryValue}", _dictionaryValue);
+            _enrichedLogger.Information("Template: {SequenceValue}", _sequence);
+            _enrichedLogger.Information("Template: {@AnonymousObject}.", _anonymousObject);
+            _enrichedLogger.Information(_exception, "Hello, {Name}!", "World");
+            _enrichedLogger.Fatal("Template2:");
+            _enrichedLogger.Fatal("Template: {ScalarStructValue}", 42);
+            _enrichedLogger.Fatal("Template2: {ScalarValue}", "42");
+            _enrichedLogger.Fatal("Template2: {DictionaryValue}", _dictionaryValue);
+            _enrichedLogger.Fatal("Template2: {SequenceValue}", _sequence);
+            _enrichedLogger.Fatal("Template2: {@AnonymousObject}.", _anonymousObject);
+            _enrichedLogger.Fatal(_exception, "Hello, {Name}! 2", "World");
+
+            return x && y;
         }
     }
 }
