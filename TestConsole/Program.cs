1c45164fdf63c5cf1fd3208c1cb00aff9753ffe1
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-11-08 10:25:06 -0800

    Add Benchmark Program

---
+++ TestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            var test = new Test();
+
+            for (int i = 0; i < 100; i++)
+            {
+                test.LogAll();
+            }
+
+            Console.WriteLine("End");
+        }
+    }
+}
---
+++ TestConsole/Test.cs
@@ -0,0 +1,153 @@
+﻿using Serilog.Events;
+using Serilog.Parsing;
+using Serilog.Core.Enrichers;
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Serilog;
+
+namespace TestConsole
+{
+    public class Test
+    {
+        readonly ILogger _logger;
+        readonly ILogger _enrichedLogger;
+        readonly LogEvent _emptyEvent;
+
+        readonly object _dictionaryValue;
+        readonly object _anonymousObject;
+        readonly object _sequence;
+        readonly Exception _exception;
+
+        public Test()
+        {
+            _exception = new Exception("An Error");
+
+            _logger = new LoggerConfiguration().CreateLogger();
+
+            _enrichedLogger = _logger.ForContext(new PropertyEnricher("Prop", "Value"));
+
+            _emptyEvent = new LogEvent(
+                DateTimeOffset.Now, 
+                LogEventLevel.Information, 
+                null, 
+                new MessageTemplate(Enumerable.Empty<MessageTemplateToken>()),
+                Enumerable.Empty<LogEventProperty>());
+
+            _anonymousObject = new
+            {
+                Level11 = "Val1",
+                Level12 = new
+                {
+                    Level21 = (int?)42,
+                    Level22 = new
+                    {
+                        Level31 = System.Reflection.BindingFlags.FlattenHierarchy,
+                        Level32 = new
+                        {
+                            X = 3,
+                            Y = "4",
+                            Z = (short?)5
+                        }
+                    }
+                }
+            };
+
+            _dictionaryValue = new Dictionary<string, object> {
+                { "Level11", "Val1" },
+                { "Level12", new Dictionary<string, object>() {
+                        { "Level21", (int?)42 },
+                        { "Level22", new Dictionary<string, object>() {
+                                { "Level31", System.Reflection.BindingFlags.FlattenHierarchy },
+                                { "Level32", new { X = 3, Y = "4", Z = (short?)5 } }
+                            }
+                        }
+                    }
+                }
+            };
+
+            _sequence = new List<object> { "1", 2, (int?)3, "4", (short)5 };
+        }
+
+        public void LogEmpty()
+        {
+            _logger.Write(_emptyEvent);
+        }
+
+        public void LogEmptyWithEnricher()
+        {
+            _enrichedLogger.Write(_emptyEvent);
+        }
+
+        
+        public void LogMsg()
+        {
+            _logger.Information("Template:");
+        }
+
+        public void LogScalar()
+        {
+            _logger.Information("Template: {ScalarValue}", "42");
+        }
+
+        public void LogScalarStruct()
+        {
+            _logger.Information("Template: {ScalarStructValue}", 42);
+        }
+
+        public void LogDictionary()
+        {
+            _logger.Information("Template: {DictionaryValue}", _dictionaryValue);
+        }
+
+        public void LogSequence()
+        {
+            _logger.Information("Template: {SequenceValue}", _sequence);
+        }
+
+        public void LogAnonymous()
+        {
+            _logger.Information("Template: {@AnonymousObject}.", _anonymousObject);
+        }
+
+        public bool LogAll()
+        {
+            var x = _logger.IsEnabled(LogEventLevel.Debug);
+            _logger.Write(_emptyEvent);
+            _logger.Information("Template:");
+            _logger.Information("Template: {ScalarStructValue}", 42);
+            _logger.Information("Template: {ScalarValue}", "42");
+            _logger.Information("Template: {DictionaryValue}", _dictionaryValue);
+            _logger.Information("Template: {SequenceValue}", _sequence);
+            _logger.Information("Template: {@AnonymousObject}.", _anonymousObject);
+            _logger.Information(_exception, "Hello, {Name}!", "World");
+            _logger.Fatal("Template2:");
+            _logger.Fatal("Template: {ScalarStructValue}", 42);
+            _logger.Fatal("Template2: {ScalarValue}", "42");
+            _logger.Fatal("Template2: {DictionaryValue}", _dictionaryValue);
+            _logger.Fatal("Template2: {SequenceValue}", _sequence);
+            _logger.Fatal("Template2: {@AnonymousObject}.", _anonymousObject);
+            _logger.Fatal(_exception, "Hello, {Name}! 2", "World");
+
+            var y = _enrichedLogger.IsEnabled(LogEventLevel.Debug);
+            _enrichedLogger.Write(_emptyEvent);
+            _enrichedLogger.Information("Template:");
+            _enrichedLogger.Information("Template: {ScalarStructValue}", 42);
+            _enrichedLogger.Information("Template: {ScalarValue}", "42");
+            _enrichedLogger.Information("Template: {DictionaryValue}", _dictionaryValue);
+            _enrichedLogger.Information("Template: {SequenceValue}", _sequence);
+            _enrichedLogger.Information("Template: {@AnonymousObject}.", _anonymousObject);
+            _enrichedLogger.Information(_exception, "Hello, {Name}!", "World");
+            _enrichedLogger.Fatal("Template2:");
+            _enrichedLogger.Fatal("Template: {ScalarStructValue}", 42);
+            _enrichedLogger.Fatal("Template2: {ScalarValue}", "42");
+            _enrichedLogger.Fatal("Template2: {DictionaryValue}", _dictionaryValue);
+            _enrichedLogger.Fatal("Template2: {SequenceValue}", _sequence);
+            _enrichedLogger.Fatal("Template2: {@AnonymousObject}.", _anonymousObject);
+            _enrichedLogger.Fatal(_exception, "Hello, {Name}! 2", "World");
+
+            return x && y;
+        }
+    }
+}
